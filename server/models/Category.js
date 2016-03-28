0b42283522b51d32ac866cc2b4eb6914f67101c0
huayun liu <huayun.liu@sap.com>
2016-03-28 16:52:54 +0800

    modify

--- server/models/Category.js
+++ server/models/Category.js
@@ -6,7 +6,7 @@ const CategorySchema = new Schema({
     subCategoryId: { type: Array },
     isNav: { type: Boolean, default: false },
     title: {type: String },
-    desc: { type: String }
+    desc: { type: String },
     src: { type: String },
     icon: { type: String }
 })
